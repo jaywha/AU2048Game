a6a553ef3e03a211b243d1f8da53b5c2c5f2ceab
AJ <ajm0050@auburn.edu>
2017-04-18 10:52:05 -0500

    added onclick events, did some testing/fixes

--- app/src/main/java/comp3710/aj/au2048game/MainActivity.java
+++ app/src/main/java/comp3710/aj/au2048game/MainActivity.java
@@ -8,17 +8,71 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import java.lang.Integer;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Controller control;
+    private Integer score;
+    private int[][] arr;
+    TextView scoreText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        scoreText = (TextView)findViewById(R.id.scoreValue);
+        control = new Controller();
+        arr = control.getArr();
     }
 
     //TODO: Make fragment for tiles : BoardView similar to CannonView?
     //TODO: Add High-score to layout : Separate Class in the model?
     //TODO: Connect controller logic to layout : Controller --> View
     //TODO: OPTIONAL :: Make UML Diagrams
+    public void UpClick(View v){
+        if(v.getId() == R.id.upArrow) {
+            control.shiftUp();
+            arr = control.getArr();
+            score = control.getScore();
+            scoreText.setText(score.toString());
+            //draw onto fragment with updated array
+        }
+    }
+    public void DownClick(View v){
+        if(v.getId() == R.id.downArrow) {
+            control.shiftDown();
+            arr = control.getArr();
+            score = control.getScore();
+            scoreText.setText(score.toString());
+            //draw onto fragment with updated array
+        }
+    }
+    public void LeftClick(View v){
+        if(v.getId() == R.id.leftArrow) {
+            control.shiftLeft();
+            arr = control.getArr();
+            score = control.getScore();
+            scoreText.setText(score.toString());
+            //draw onto fragment with updated array
+        }
+    }
+    public void RightClick(View v){
+        if(v.getId() == R.id.rightArrow) {
+            control.shiftRight();
+            arr = control.getArr();
+            score = control.getScore();
+            scoreText.setText(score.toString());
+            //draw onto fragment with updated array
+        }
+    }
+    public void ResetClick(View v){
+        if(v.getId() == R.id.restartButton) {
+            control.reset();
+            arr = control.getArr();
+            score = 0;
+            scoreText.setText(score.toString());
+            //draw onto fragment with updated array
+        }
+    }
 }
